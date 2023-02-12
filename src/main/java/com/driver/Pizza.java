e41da774219a207f7a4c8c03c7aff24dee97246c
Kunal Kasera <kunalkasera456@gmail.com>
2023-02-12 23:28:13 +0530

    first commit

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -6,9 +6,31 @@ public class Pizza {
     private Boolean isVeg;
     private String bill;
 
+    private int toppings;
+    boolean isExtraCheesAdded;
+    boolean isExtraTopingsAdded;
+    boolean isTakeaway;
+    boolean isBillCreated;
+
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
+        if(isVeg)
+        {
+            this.price = 300;
+            this.toppings = 70;
+        }
+        else
+        {
+            this.price = 400;
+            this.toppings = 120;
+        }
+        isExtraCheesAdded = false;
+        isExtraTopingsAdded = false;
+        isTakeaway = false;
+        isBillCreated = false;
+
+        this.bill = "Base Price Of The Pizza: " + this.price +"\n";
     }
 
     public int getPrice(){
@@ -17,18 +39,47 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
+        if(!isExtraCheesAdded)
+        {
+            this.price += 80;
+            this.isExtraCheesAdded = true;
+        }
     }
 
     public void addExtraToppings(){
         // your code goes here
+        if(!isExtraTopingsAdded)
+        {
+            this.price += this.toppings;
+            isExtraTopingsAdded = true;
+        }
     }
 
     public void addTakeaway(){
         // your code goes here
+        if(!isTakeaway)
+        {
+            this.price += 20;
+            isTakeaway = true;
+        }
     }
 
     public String getBill(){
         // your code goes here
-        return this.bill;
+        if(!isBillCreated) {
+            if (isExtraCheesAdded == true) {
+                this.bill += "Extra Cheese Added: 80" + "\n";
+            }
+            if (isExtraTopingsAdded == true) {
+                this.bill += "Extra Toppings Added: " + this.toppings + "\n";
+            }
+            if (isTakeaway == true) {
+                this.bill += "Paperbag Added: 20"+"\n";
+            }
+            this.bill = "Total Price: "+this.price;
+            isBillCreated = true;
+            return this.bill;
+        }
+        return "";
     }
 }
